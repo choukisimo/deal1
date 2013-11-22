b179e7ef6b5f3e19402c3a1f5551ff6ba9be4754
Haithem Souala <HaithemSouala@users.noreply.github.com>
2013-11-22 19:06:57 +0000

    Create conf.php

---
+++ core/conf.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Permet de recuperer la configuration mysql
+*/
+class Conf{
+
+    
+    static $databases = array(
+        'default' => array(
+            'host' => 'localhost',
+            'database' => 'deal',
+            'login' => 'root',
+            'password' => ''
+        )
+    );
+}
+?>
